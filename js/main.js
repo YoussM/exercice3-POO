5fe2a59556b2f7e59f127bb43e74fc9fd983116f
YoussM <youssef.mercier@hotmail.fr>
2017-08-23 16:44:04 +0200

    fin fichier poo

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var tab = [];
+var indice = 0;
+
+function formulaire (titre,phrase,description, ){
+  this.titre = titre;
+  this.phrase = phrase;
+  this.description = description;
+
+}
+
+function objetContenuInput(){
+  this.mesObjet = function(){
+    var contenuInput = new formulaire ($("#titre").val(),$("#phrase").val(),$("#descript").val());
+    tab.push(contenuInput);
+
+  }
+  this.mesAffichages = function(){
+  $(".carte").append("<div class='affichage'><p class='background'>" + tab[indice].titre + "</p><p class='background'>" + tab[indice].phrase + "</p><p class='background'>" + tab[indice].description + "</p></div>")
+  indice++;
+ }
+
+}
+
+$("#contact-submit").click(function(){
+var recupObjet = new objetContenuInput();
+recupObjet.mesObjet();
+recupObjet.mesAffichages();
+})
